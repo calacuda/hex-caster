0fa64a18da2b482493a45b1dcfdaa7378fd759af
Eoghan West <eowest@gmail.com>
2026-01-04 14:58:43 -0500

    added command recv announcment

--- justfile
+++ justfile
@@ -2,4 +2,4 @@ _:
   @just -l
 
 mon port:
-  rlwrap --always-readline picocom -b 115200 {{port}}
+  rlwrap --always-readline --no-child --ansi-colour-aware picocom -cqb 115200 {{port}}
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,27 @@
 #![no_std]
 #![no_main]
 
-// #[macro_use]
-// extern crate alloc;
+#[macro_use]
+extern crate alloc;
 
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
 use embassy_executor::Spawner;
 use embassy_rp::{
     peripherals::USB,
     usb::{Driver, InterruptHandler},
     {bind_interrupts, gpio},
 };
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
 use embassy_time::{Duration, Timer};
 use embassy_usb::{
     class::cdc_acm::{CdcAcmClass, State},
     {Builder, Config},
 };
+use embassy_usb_logger::ReceiverHandler;
+use embedded_alloc::LlffHeap as Heap;
 use gpio::{Level, Output};
 use log::*;
 use {defmt_rtt as _, panic_probe as _};
@@ -34,12 +41,52 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
 
+#[allow(static_mut_refs)]
+fn init_heap() {
+    use core::mem::MaybeUninit;
+    static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+    unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+}
+
 bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => InterruptHandler<USB>;
 });
 
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+const HEAP_SIZE: usize = 128 * 1024;
+
+static COMMAND_CHANNEL: Channel<CriticalSectionRawMutex, String, 4> = Channel::new();
+
+pub struct CmdHandler {}
+
+impl ReceiverHandler for CmdHandler {
+    fn new() -> Self {
+        Self {}
+    }
+
+    async fn handle_data(&self, data: &[u8]) {
+        match core::str::from_utf8(data) {
+            Ok(cmd) => {
+                info!("recv a command {cmd}");
+                // let mut buf = [0u8; 256];
+                COMMAND_CHANNEL.send(cmd.to_string()).await;
+
+                if cmd.starts_with("/greet ") {
+                    let name = &cmd[7..cmd.len()];
+
+                    info!("Hello, {name}!");
+                }
+            }
+            Err(e) => error!("messeage failed to parse with error: {e}. (likely invalid utf8)"),
+        };
+    }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    init_heap();
+
     let p = embassy_rp::init(Default::default());
     let driver = Driver::new(p.USB, Irqs);
     // spawner.spawn(logger_task(driver)).unwrap();
@@ -50,7 +97,7 @@ async fn main(spawner: Spawner) {
     // Delay::delay_ms(&mut embassy_time::Delay, 500_u32);
     Timer::after(Duration::from_millis(1000)).await;
 
-    info!("Hello, World!");
+    // info!("Hello, World!");
     // loop {
     //     led.set_high();
     //     debug!("on");
@@ -111,17 +158,37 @@ async fn logger_task(driver: Driver<'static, USB>) {
     // Run the USB device.
     let usb_fut = usb.run();
 
-    let log_fut = embassy_usb_logger::with_custom_style!(
-        1024,
-        log::LevelFilter::Debug,
-        logger_class,
-        |record, writer| {
-            use core::fmt::Write;
-            let level = record.level().as_str();
-            write!(writer, "[{level}] {}\r\n", record.args()).unwrap();
-        }
-    );
+    // let log_fut = embassy_usb_logger::with_custom_style!(
+    //     1024,
+    //     log::LevelFilter::Debug,
+    //     logger_class,
+    //     |record, writer| {
+    //         use core::fmt::Write;
+    //         let level = record.level().as_str();
+    //         write!(writer, "[{level}] {}\r\n", record.args()).unwrap();
+    //     }
+    // );
+    #[allow(static_mut_refs)]
+    let log_fut = unsafe {
+        static mut LOGGER: ::embassy_usb_logger::UsbLogger<1024, CmdHandler> =
+            ::embassy_usb_logger::UsbLogger::with_custom_style(|record, writer| {
+                use core::fmt::Write;
+                let level = record.level().as_str();
+                if record.target().starts_with(env!("CARGO_PKG_NAME")) {
+                    write!(writer, "[{level}] {}\r\n", record.args(),).unwrap();
+                } else {
+                    // write!(writer, "[{level}] |{}|\r\n", env!("CARGO_PKG_NAME"),).unwrap();
+                }
+            });
+        LOGGER.with_handler(CmdHandler::new());
+        let _ = ::log::set_logger_racy(&LOGGER)
+            .map(|()| log::set_max_level_racy(log::LevelFilter::Debug));
+
+        LOGGER.create_future_from_class(logger_class)
+    };
+
     // log_fut.await
+
     // TODO: add other usb handling here
     embassy_futures::join::join(
         // futures that reutrn -> ()
